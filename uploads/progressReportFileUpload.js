626f4ce471dced18c74f3397570f8c106a36b02f
Your Name <you@example.com>
2025-01-28 21:21:44 +0530

    28-01-2025(Tuesday)

--- uploads/progressReportFileUpload.js
+++ uploads/progressReportFileUpload.js
@@ -24,7 +24,7 @@ const progressReportFileUpload = multer({
       const day = String(now.getDate()).padStart(2, "0");
       const month = String(now.getMonth() + 1).padStart(2, "0");
       const year = now.getFullYear();
-      const formattedDate = `${day}--${month}--${year}`;
+      const formattedDate = `${day}-${month}-${year}`;
       const fileExtension = path.extname(file.originalname); // Extract file extension
       const fileBaseName = path
         .basename(file.originalname, fileExtension)
